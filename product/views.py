9bcd0abd0612c66d10265f030924c571b5f434a2
Alim Maratov <alimmaratov11@gmail.com>
2025-12-16 18:35:45 +0600

    hw1

---
+++ product/views.py
@@ -0,0 +1,56 @@
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
+from .models import Category, Product, Review
+from .serializers import CategoryListSerializer, CategoryDetailSerializer , ProductListSerializer, ProductDetailSerializer, ReviewListSerializer, ReviewDetailSerializer
+
+
+@api_view(['GET'])
+def category_list_api_view(request):
+    categories = Category.objects.all()
+    list_ = CategoryListSerializer(categories, many=True).data
+    return Response(list_, status.HTTP_200_OK)
+
+@api_view(['GET'])
+def category_detail_api_view(request, id):
+    try:
+        categories = Category.objects.get(id=id)
+    except Category.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND,
+                        data={"error": "Category not found!"})
+    list_ = CategoryDetailSerializer(categories, many=False).data
+    return Response(list_, status.HTTP_200_OK)
+
+
+@api_view(['GET'])
+def product_list_api_view(request):
+    products = Product.objects.all()
+    list_ = ProductListSerializer(products, many=True).data
+    return Response(list_, status.HTTP_200_OK)
+
+@api_view(['GET'])
+def product_detail_api_view(request, id):
+    try:
+        products = Product.objects.get(id=id)
+    except Product.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND,
+                        data={"error": "Category not found!"})
+    list_ = ProductDetailSerializer(products, many=False).data
+    return Response(list_, status.HTTP_200_OK)
+
+
+@api_view(['GET'])
+def review_list_api_view(request):
+    reviews = Review.objects.all()
+    list_ = ReviewListSerializer(reviews, many=True).data
+    return Response(list_, status.HTTP_200_OK)
+
+@api_view(['GET'])
+def review_detail_api_view(request, id):
+    try:
+        reviews = Review.objects.get(id=id)
+    except Review.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND,
+                        data={"error": "Category not found!"})
+    list_ = ReviewDetailSerializer(reviews, many=False).data
+    return Response(list_, status.HTTP_200_OK)
